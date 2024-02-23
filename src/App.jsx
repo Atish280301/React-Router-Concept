854a4a84acc597612500ef5f4ccf56b309fd26eb
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-02-23 18:41:51 +0530

    Commit - 05
    Add Switch Concept In Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import WelcomeFile from "./Components/WelcomeFile";
 import Products from "./Components/Products";
 import MainHeader from "./Component/MainHeader";
+import ProductDetail from "./Components/ProductDetail";
 const App = () => {
   return (
     <>
       <MainHeader />
       <main>
-        <Route path="/Welcome">
-          <WelcomeFile />
-        </Route>
-        <Route path="/Product">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/Welcome" exact>
+            <WelcomeFile />
+          </Route>
+          <Route path="/Product" exact>
+            <Products />
+          </Route>
+          <Route path="/Product/:productId" exact>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </>
   );
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const Products = () => {
     return (
         <>
             <h1>Welcome To Product Page</h1>
+            <div><Link to="/Product/p1">Book</Link></div>
+            <div><Link to="/Product/p2">Pen</Link></div>
+            <div><Link to="/Product/p3">Calculator</Link></div>
         </>
     );
 }
